413e4ada315dbbec52d50e0532803ea6d51a1e87
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-20 10:37:20 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     // Compose
     implementation ("androidx.activity:activity-compose:1.8.2")
-    implementation platform(")androidx.compose:compose-bom:2023.10.01"))
+    implementation platform(androidx.compose:compose-bom:2023.10.01")
     implementation ("androidx.compose.ui:ui")
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
